9e1fef093f3f87b686ae2d70e74bae6d1b188aaf
chabaniuk-m <chabanyuk.maks@gmail.com>
2024-01-29 10:47:41 +0200

    звʼязний список: більше заголовків методів

--- src/ds/list/LinkedList.java
+++ src/ds/list/LinkedList.java
@@ -22,7 +22,6 @@ public class LinkedList {
      * @param element що хочете додати до списку
      */
     void add(String element){
-        // TODO: add(element)
         Node newNode = new Node();
         newNode.value = element;
 
@@ -37,24 +36,14 @@ public class LinkedList {
     }
 
     /**
-     * Додавання нового елементу за індексом;
-     * @param element що хочете додати
-     * @param index індекс нового елементу
-     */
-    void add(String element, int index){
-        // TODO: add(element, index)
-    }
-
-    /**
      * Отримання першого елемента
      * @return перший елемент списку
      */
     String getFirst() {
-        // TODO: getFirst()
         if (head != null) {
             return head.value;
         }
-        return null;
+        throw new IndexOutOfBoundsException();
     }
 
     /**
@@ -62,11 +51,10 @@ public class LinkedList {
      * @return останній елемент списку
      */
     String getLast(){
-        // TODO: getLast()
         if (tail != null) {
             return tail.value;
         }
-        return null;
+        throw new IndexOutOfBoundsException();
     }
 
     /**
@@ -94,5 +82,117 @@ public class LinkedList {
             return true;
         }
         return false;
+    }
+
+    /**
+     * @return кількість елементів у списку (найменше – 0)
+     */
+    int size() {
+        // TODO: size()
+        return 0;
+    }
+
+    /**
+     * Шукає індекс елемента
+     * @param element елемент індекс якого шукаємо
+     * @return індекс елементу, якщо елемент не знайдений, повертає -1
+     */
+    int indexOf(String element) {
+        // TODO: indexOf(element)
+        return -1;
+    }
+
+
+    /**
+     * Знаходить останній індекс елементу (останній раз коли елемент зустрічається у списку)
+     * @param element елемент
+     * @return останній індекс елементу, якщо елемент не знайдений, повертає -1
+     */
+    int lastIndexOf(String element) {
+        // TODO: lastIndexOf(element)
+        return -1;
+    }
+
+    /**
+     * Видаляє елемент зі списку за індексом. Створюється помилка, якщо індекс виходить за межі
+     * @param index
+     * @return значення, яке було видалено
+     */
+    String remove(int index) {
+        // TODO: remove(index)
+        throw new IndexOutOfBoundsException();
+    }
+
+    // TODO: видалення першого та останнього елемента
+
+    /**
+     * Видаляє перший елемент списку
+     * @return видаленний елемент
+     */
+    String removeFirst(){
+        // TODO: removeFirst()
+        return null;
+    }
+
+    /**
+     * Видаляє останній елемент списку
+     * @return видаленний елемент
+     */
+    String removeLast(){
+        // TODO: removeLast()
+        return null;
+    }
+
+    /**
+     * Робить заміну елементу, cтворюється помилка, якщо індекс виходить за межі
+     * @param index куди замінити елемент
+     * @param newValue нове значення
+     * @return попередній елемент
+     */
+    String set(int index, String newValue) {
+        // TODO: set(index, newValue)
+        return null;
+    }
+
+    /**
+     * Додавання нового елементу за індексом;
+     * @param element що хочете додати
+     * @param index індекс нового елементу
+     */
+    void add(String element, int index){
+        // TODO: add(element, index)
+    }
+
+    /**
+     * Додає новий елемент у початок списку
+     * @param element елемент який ми хочемо додати
+     */
+    void addFirst(String element){
+        // TODO: addFirst()
+    }
+
+    /**
+     * Додає новий елемент у кінець списку
+     * @param element елемент який ми хочемо додати
+     */
+    void addLast(String element){
+        // TODO: addLast()
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) {
+            return "[]";
+        }
+        StringBuilder sb = new StringBuilder("[");
+        sb.append(head.value);
+        Node current = head.next;
+        while (current != null) {
+            sb.append(", ");
+            sb.append(current.value);
+            current = current.next;
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
